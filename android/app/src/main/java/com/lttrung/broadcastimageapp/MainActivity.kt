7cfe31d61534709ae96cc620cafa918b38cdc23d
Le Thanh Trung <61788629+lttrung2001@users.noreply.github.com>
2023-05-16 12:08:24 +0700

    up

---
+++ android/app/src/main/java/com/lttrung/broadcastimageapp/MainActivity.kt
@@ -0,0 +1,230 @@
+package com.lttrung.broadcastimageapp
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.util.Size
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.MutableLiveData
+import com.hivemq.client.internal.mqtt.datatypes.MqttUtf8StringImpl
+import com.hivemq.client.internal.mqtt.message.auth.MqttSimpleAuth
+import com.hivemq.client.mqtt.MqttClient
+import com.hivemq.client.mqtt.mqtt5.Mqtt5AsyncClient
+import com.lttrung.broadcastimageapp.databinding.ActivityMainBinding
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+@SuppressLint("RestrictedApi")
+class MainActivity : AppCompatActivity() {
+    private val viewBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private val imageCapture: ImageCapture by lazy {
+        ImageCapture.Builder().setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+            .setMaxResolution(Size.parseSize("1000x1000")).build()
+    }
+    private val cameraExecutor: ExecutorService by lazy {
+        Executors.newSingleThreadExecutor()
+    }
+    private val imageLiveData: MutableLiveData<ByteArray> by lazy {
+        MutableLiveData<ByteArray>()
+    }
+
+    //    private val socket: Socket by lazy {
+//        Socket("192.168.1.88", 8080)
+//    }
+    private val client: Mqtt5AsyncClient by lazy {
+        MqttClient.builder().identifier(UUID.randomUUID().toString()).serverHost(BROKER)
+            .serverPort(PORT).useMqttVersion5().sslWithDefaultConfig().simpleAuth(
+                MqttSimpleAuth(
+                    MqttUtf8StringImpl.of(USERNAME),
+                    ByteBuffer.wrap(PASSWORD.toByteArray(Charsets.UTF_8))
+                )
+            ).build().toAsync()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(viewBinding.root)
+        client.connect().whenComplete { connAck, t ->
+            if (t != null) {
+                t.printStackTrace()
+            } else {
+                Log.i("INFO", "CONNECTED")
+            }
+        }
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+        // Set up the listeners for take photo and video capture buttons
+        viewBinding.imageCaptureButton.setOnClickListener {
+//            client.publishWith().topic("topic2").payload("hehe".toByteArray(Charsets.UTF_8)).send().whenComplete { connAck, t ->
+//                if (t != null) {
+//                    t.printStackTrace()
+//                } else {
+//                    Log.i("INFO", "PUBLISHED")
+//                }
+//            }
+            takePhoto()
+        }
+        // Set up observer
+        imageLiveData.observe(this) {
+            // Send image to server, remove photo and take a new photo
+            Log.i("INFO", it.size.toString())
+            //                socket.getOutputStream().write(it)
+            client.publishWith().topic("topic2").payload(it).send().whenComplete { ack, t ->
+                if (t != null) {
+                    t.printStackTrace()
+                } else {
+                    Log.i("INFO", "PUBLISHED")
+                    takePhoto()
+                }
+            }
+        }
+    }
+
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture
+
+        // Create time stamped name and MediaStore entry.
+        val name = SimpleDateFormat(FILENAME_FORMAT).format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            // Từ android 10 (lớn hơn 9) trở lên phải set RELATIVE PATH cho ảnh
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                // Dòng code bên dưới có nghĩa là lưu ảnh vào album riêng trong Pictures tên CameraX-Image
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/SelfDrivingCar")
+            }
+        }
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(
+            contentResolver, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues
+        )
+            .build()
+
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val msg = "Photo capture succeeded: ${output.savedUri}"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    val inputStream = contentResolver.openInputStream(output.savedUri!!)
+                    val bytes = inputStream!!.readBytes()
+                    imageLiveData.postValue(bytes)
+                    inputStream.close()
+                }
+            }
+        )
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
+                }
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture
+                )
+            } catch (exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "SelfDrivingCarApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = mutableListOf(
+            Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO
+        ).apply {
+            // Android 9 trở xuống phải cấp quyền write bộ nhớ ngoài
+            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+        }.toTypedArray()
+
+        private const val BROKER = "4a05b8e1d6604c8c8c84238e32f702e8.s2.eu.hivemq.cloud"
+        private const val PORT = 8883
+        private const val USERNAME = "n19dccn214"
+        private const val PASSWORD = "ejURUQaYZWn2@t"
+    }
+}
