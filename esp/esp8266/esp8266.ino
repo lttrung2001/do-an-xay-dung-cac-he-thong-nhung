ffa6edf2b053a7754e193496f0f3e6fad84a29a2
Le Thanh Trung <61788629+lttrung2001@users.noreply.github.com>
2023-05-17 20:18:06 +0700

    Update esp8266.ino

--- esp/esp8266/esp8266.ino
+++ esp/esp8266/esp8266.ino
@@ -2,7 +2,10 @@
 #include <PubSubClient.h>
 #include <string>
 #include <string.h>
-#include <Wire.h>
+
+#include <Servo.h>
+#include <Ultrasonic.h>
+#include <L298N.h>
 
 #define WIFI_SSID "THANHTRUNG"
 #define WIFI_PASSWORD "12345678"
@@ -15,22 +18,61 @@
 WiFiClientSecure wifiClient;
 PubSubClient client(wifiClient);
 
+int distance = 0;
+const int trigPin = D6;
+const int echoPin = D5;
+Ultrasonic ultrasonic(trigPin, echoPin);
+
+const int servoPin = D7;
+Servo servo;
+
+const int IN1 = D1;
+const int IN2 = D2;
+const int IN3 = D3;
+const int IN4 = D4;
+L298N leftMotor(IN1, IN2);
+// Right motor can not backward
+L298N rightMotor(IN3, IN4);
+
 void setup() {
   pinMode(LED_BUILTIN, HIGH);
   // put your setup code here, to run once:
-  Wire.begin(D1, D2);
   setupWifi();
   wifiClient.setInsecure();
   client.setServer(MQTT_SERVER, MQTT_PORT);
   client.setCallback(callback);
   connectToBroker();
+
+  servo.attach(servoPin);
+
   Serial.begin(9600);
-  Serial.println("Start transfer");
 }
 
 void loop() {
-  client.loop();
   // put your main code here, to run repeatedly:
+  // client.loop();
+  int leftDistance = 0;
+  int rightDistance = 0;
+  // Read distance
+  distance = ultrasonic.read();
+  delay(100);
+  Serial.print("Front: ");
+  Serial.println(distance);
+  if (distance <= 30) {
+    stopAllMotors();
+    // goBack();
+    // stopAllMotors();
+    leftDistance = lookLeft();
+    rightDistance = lookRight();
+    lookFront();
+    if (leftDistance > rightDistance) {
+      turnLeft();
+    } else {
+      turnRight();
+    }
+  } else {
+    goForward();
+  }
 }
 
 void setupWifi() {
@@ -65,7 +107,56 @@ void connectToBroker() {
 void callback(char* topic, byte *payload, unsigned int length) {
   Serial.println(topic);
   Serial.write(payload, length);
-  Wire.beginTransmission(8);
-  Wire.write(1);
-  Wire.endTransmission();
+}
+
+void stopAllMotors() {
+  leftMotor.stop();
+  rightMotor.stop();
+  delay(100);
+}
+
+void goForward() {
+  leftMotor.forward();
+  rightMotor.forward();
+}
+
+void goBack() {
+  leftMotor.backward();
+  rightMotor.backward();
+  delay(500);
+}
+
+void turnLeft() {
+  leftMotor.backward();
+  delay(500);
+}
+
+void turnRight() {
+  leftMotor.forward();
+  delay(500);
+}
+
+int lookLeft() {
+  servo.write(120);
+  delay(200);
+  int leftDistance = ultrasonic.read();
+  delay(100);
+  Serial.print("Left: ");
+  Serial.println(leftDistance);
+  return leftDistance;
+}
+
+int lookRight() {
+  servo.write(60);
+  delay(200);
+  int rightDistance = ultrasonic.read();
+  delay(100);
+  Serial.print("Right: ");
+  Serial.println(rightDistance);
+  return rightDistance;
+}
+
+void lookFront() {
+  servo.write(90);
+  delay(500);
 }
