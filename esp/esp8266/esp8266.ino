99f640fe800854787d22d6f7297055398b8afa2f
Le Thanh Trung <61788629+lttrung2001@users.noreply.github.com>
2023-05-29 13:15:58 +0700

    write comment for esp code

--- esp/esp8266/esp8266.ino
+++ esp/esp8266/esp8266.ino
@@ -7,44 +7,66 @@
 #include <Ultrasonic.h>
 #include <L298N.h>
 
+// WIFI parameters
 #define WIFI_SSID "THANHTRUNG"
 #define WIFI_PASSWORD "12345678"
 
+// MQTT HiveMQ parameters
 #define MQTT_SERVER "4a05b8e1d6604c8c8c84238e32f702e8.s2.eu.hivemq.cloud"
 #define MQTT_PORT 8883
 #define MQTT_USER "n19dccn214"
 #define MQTT_PASSWORD "ejURUQaYZWn2@t"
 
+// Using to connect mqtt broker.
 WiFiClientSecure wifiClient;
 PubSubClient client(wifiClient);
 
+// Using to store distance value in centimeter.
 int distance = 0;
+// Define trigger and echo pins of ultrasonic sensor.
 const int trigPin = D6;
 const int echoPin = D5;
+// Declare ultrasonic object
 Ultrasonic ultrasonic(trigPin, echoPin);
 
+// Define data pin of servo.
 const int servoPin = D7;
+// Declare servo object.
+// 90 degree is look forward.
+// 60 degree is look left.
+// 120 degree is look right.
 Servo servo;
 
+// Left back motor.
 const int IN1 = D1;
 const int IN2 = D2;
+
+// Right back motor.
 const int IN3 = D3;
 const int IN4 = D4;
+
+// Declare motor objects.
 L298N leftMotor(IN1, IN2);
-// Right motor can not backward
 L298N rightMotor(IN3, IN4);
 
+// Status of car.
+// true: self-driving mode.
+// false: mobile control mode.
 bool isSelfDriving = true;
 
 void setup() {
   pinMode(LED_BUILTIN, HIGH);
-  // put your setup code here, to run once:
   setupWifi();
+  // Why to set insecure to wifi client here?
   wifiClient.setInsecure();
+  // Set server parameters to connect to mqtt service in the future.
   client.setServer(MQTT_SERVER, MQTT_PORT);
+  // Set callback.
   client.setCallback(callback);
+  // Connect to mqtt broker.
   connectToBroker();
 
+  // Attach servo using servo pin as defined previously.
   servo.attach(servoPin);
 
   Serial.begin(9600);
@@ -53,26 +75,37 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   client.loop();
+  // If status is self-driving mode.
   if (isSelfDriving) {
+    // Then create 2 int store left and right distance value.
     int leftDistance = 0;
     int rightDistance = 0;
-    // Read distance
+    // Read front distance.
     distance = ultrasonic.read();
     delay(100);
-    if (distance <= 30) {
+    // If front distance is smaller than the value we defined (30 cm).
+    if (distance < 30) {
+      // Stop all the motors.
       stopAllMotors();
+      // Then look left and right to get left and right distance.
       leftDistance = lookLeft();
       rightDistance = lookRight();
+      // After look left and right then make servo look straight forward to reset the state.
       lookFront();
+      // Which direction has a longer distance, then go in that direction.
       if (leftDistance > rightDistance) {
         turnLeft();
       } else {
         turnRight();
       }
-    } else {
+    }
+    // If distance is equals or greater than defined value then forward normally. 
+    else {
       goForward();
     }
-  } else {
+  }
+  // If status is mobile control mode then stop all the motors and wait for control signals.
+  else {
     stopAllMotors();
   }
 }
@@ -90,15 +123,22 @@ void setupWifi() {
 }
 
 void connectToBroker() {
+  // Try to connect mqtt broker until client connected.
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     String clientId = "ESP8266";
     clientId += String(random(0xffff), HEX);
+    // Client connected
     if (client.connect(clientId.c_str(), MQTT_USER, MQTT_PASSWORD)) {
+      // Then subscribe needed topics.
       Serial.println("connected");
+      // We subscribe "topic2" to receive....
       client.subscribe("topic2");
+      // We subscribe "control" to receive control signals.
       client.subscribe("control");
-    } else {
+    } 
+    // If client connect failed, try again after 2 seconds.
+    else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 2 seconds");
@@ -107,34 +147,49 @@ void connectToBroker() {
   }
 }
 
+// This method use to listen incoming data from subcribed topics.
 void callback(char* topic, byte* payload, unsigned int length) {
+  // Topic "control".
+  // Receive value:
+  // 1: Left
+  // 2: Forward
+  // 3: Right
+  // 4: Backward
+  // Other values: switch control mode.
   if (strcmp(topic, "control") == 0) {
+    // Parse received value
     char value[1];
     value[0] = payload[0];
-    Serial.println(String(value));
+    // If value is 1 then the car move to left.
     if (String(value).substring(0,1) == "1") {
       leftMotor.backward();
       rightMotor.forward();
       delay(100);
-    } else if (String(value).substring(0,1) == "3") {
+    } 
+    // Move to right.
+    else if (String(value).substring(0,1) == "3") {
       leftMotor.forward();
       rightMotor.backward();
       delay(100);
-    } else if (String(value).substring(0,1) == "2") {
+    } 
+    // Move forward.
+    else if (String(value).substring(0,1) == "2") {
       leftMotor.forward();
       rightMotor.forward();
       delay(100);
-    } else if (String(value).substring(0,1) == "4") {
+    } 
+    // Move backward.
+    else if (String(value).substring(0,1) == "4") {
       leftMotor.backward();
       rightMotor.backward();
       delay(100);
-    } else {
+    } 
+    // Switch mode.
+    else {
       isSelfDriving = !isSelfDriving;
     }
     return;
   }
-  Serial.println(topic);
-  Serial.write(payload, length);
 }
 
 void stopAllMotors() {
@@ -166,7 +221,7 @@ void turnRight() {
 
 int lookLeft() {
   servo.write(120);
-  delay(200);
+  delay(100);
   int leftDistance = ultrasonic.read();
   delay(100);
   return leftDistance;
@@ -174,7 +229,7 @@ int lookLeft() {
 
 int lookRight() {
   servo.write(60);
-  delay(200);
+  delay(100);
   int rightDistance = ultrasonic.read();
   delay(100);
   return rightDistance;
@@ -182,5 +237,5 @@ int lookRight() {
 
 void lookFront() {
   servo.write(90);
-  delay(500);
+  delay(100);
 }
