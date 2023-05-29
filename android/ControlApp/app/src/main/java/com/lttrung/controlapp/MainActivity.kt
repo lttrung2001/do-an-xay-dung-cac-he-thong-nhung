f5585fe8d8085403351782de68c64c6b38d6b2f1
Le Thanh Trung <61788629+lttrung2001@users.noreply.github.com>
2023-05-29 13:23:52 +0700

    edit name of control app

--- android/ControlApp/app/src/main/java/com/lttrung/controlapp/MainActivity.kt
+++ android/ControlApp/app/src/main/java/com/lttrung/controlapp/MainActivity.kt
@@ -18,7 +18,9 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    // Build a client with parameters to connect mqtt broker in the future.
     private val client: Mqtt5AsyncClient by lazy {
+        // Build with server address, port, username, password.
         MqttClient.builder().identifier(UUID.randomUUID().toString()).serverHost(BROKER)
             .serverPort(PORT).useMqttVersion5().sslWithDefaultConfig().simpleAuth(
                 MqttSimpleAuth(
@@ -31,35 +33,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
+        // Connect to mqtt broker async
         client.connect().whenComplete { connAck, t ->
+            // If connect failed then print stack trace.
             if (t != null) {
                 t.printStackTrace()
-            } else {
+            }
+            // Connected.
+            else {
                 Log.i("INFO", "CONNECTED")
                 viewBinding.buttonChangeStatus.setOnClickListener {
                     client.publishWith().topic("control").payload("0".toByteArray()).send()
                 }
-                viewBinding.buttonLeft.setOnTouchListener { v, event ->
+                viewBinding.buttonLeft.setOnClickListener {
                     client.publishWith().topic("control").payload("1".toByteArray()).send()
-                    true
                 }
-                viewBinding.buttonRight.setOnTouchListener { v, event ->
+                viewBinding.buttonRight.setOnClickListener {
                     client.publishWith().topic("control").payload("3".toByteArray()).send()
-                    true
                 }
-                viewBinding.buttonForward.setOnTouchListener { v, event ->
+                viewBinding.buttonForward.setOnClickListener {
                     client.publishWith().topic("control").payload("2".toByteArray()).send()
-                    true
                 }
-                viewBinding.buttonBackward.setOnTouchListener { v, event ->
+                viewBinding.buttonBackward.setOnClickListener {
                     client.publishWith().topic("control").payload("4".toByteArray()).send()
-                    true
                 }
             }
         }
     }
 
     companion object {
+        // Define mqtt parameters.
         private const val BROKER = "4a05b8e1d6604c8c8c84238e32f702e8.s2.eu.hivemq.cloud"
         private const val PORT = 8883
         private const val USERNAME = "n19dccn214"
