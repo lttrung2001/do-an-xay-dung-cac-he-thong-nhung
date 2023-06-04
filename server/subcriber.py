5353501fecb65314b0e0403dc1a7bf310bd33cd3
Le Thanh Trung <61788629+lttrung2001@users.noreply.github.com>
2023-06-04 10:17:35 +0700

    Update subcriber.py

--- server/subcriber.py
+++ server/subcriber.py
@@ -10,6 +10,24 @@ username= "n19dccn018"
 password= "ejURUQaYZWn2@t"
 topic= "topic2"
 
+
+
+from camera_calibration import calib, undistort
+from threshold import get_combined_gradients, get_combined_hls, combine_grad_hls
+from line import Line, get_perspective_transform, get_lane_lines_img, illustrate_driving_lane, illustrate_info_panel, illustrate_driving_lane_with_topdownview
+from process import pipeline
+left_line = Line()
+right_line = Line()
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#   Tune Parameters for different inputs        #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+th_sobelx, th_sobely, th_mag, th_dir = (35, 100), (30, 255), (30, 255), (0.7, 1.3)
+th_h, th_l, th_s = (10, 100), (0, 60), (85, 255)
+
+# camera matrix & distortion coefficient
+mtx, dist = calib()
+
 # Define callback functions for events
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
@@ -19,9 +37,9 @@ def on_message(client, userdata, message):
     # print("Received message: " + message.payload.decode())
     image = np.asarray(bytearray(message.payload), dtype="uint8")
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    cv2.imshow("Test", image)
-    # cv2.waitKey(0)
-    # print( message.payload)
+    # Image processing
+    result_image, direction = pipeline(image)
+    client.publish("output", str(direction))
 
 # Create an MQTT client
 client = paho.Client(client_id="456")
